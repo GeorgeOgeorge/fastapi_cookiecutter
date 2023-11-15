537809f89869edd23d55d5b93e18e41ae8d044b5
george morais <george.morais@sensedata.com.br>
2023-11-15 00:11:03 -0300

    feat: creates venv and runs it

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,8 @@
+import os
 import subprocess
-import sys
 
-subprocess.run([sys.executable, "../scripts/init_project.py"])
+project_directory = os.getcwd()
+
+subprocess.run(["python", "-m", "venv", "venv"], cwd=project_directory, check=True)
+subprocess.run(["bash", "../scripts/run_venv.sh"], cwd=project_directory, check=True)
+subprocess.run(["pip", "install", "-r", "requirements.txt"], cwd=project_directory, check=True)
