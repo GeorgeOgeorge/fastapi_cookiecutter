5f63ac07fee08d4609f9d5ff26d45e0236690b32
george morais <george.morais@sensedata.com.br>
2023-11-15 00:33:02 -0300

    feat: checks if required values are missing

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,8 +1,20 @@
 import os
+import sys
 import subprocess
 
-project_directory = os.getcwd()
+required_vars = (
+    "{{ cookiecutter.db_user }}",
+    "{{ cookiecutter.db_pass }}",
+    "{{ cookiecutter.db_host }}",
+    "{{ cookiecutter.db_port }}",
+)
 
-subprocess.run(["python", "-m", "venv", "venv"], cwd=project_directory, check=True)
-subprocess.run(["bash", "../scripts/run_venv.sh"], cwd=project_directory, check=True)
-subprocess.run(["pip", "install", "-r", "requirements.txt"], cwd=project_directory, check=True)
+if None in required_vars:
+    print("Some required values are missing please check inputs.")
+    sys.exit(1)
+else:
+    project_directory = os.getcwd()
+
+    subprocess.run(["python", "-m", "venv", "venv"], cwd=project_directory, check=True)
+    subprocess.run(["bash", "../scripts/run_venv.sh"], cwd=project_directory, check=True)
+    subprocess.run(["pip", "install", "-r", "requirements.txt"], cwd=project_directory, check=True)
